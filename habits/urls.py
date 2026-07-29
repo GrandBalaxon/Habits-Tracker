f1bdd6bd2322c28d07027675304609855a23e5cd
Vlad Spinul <shinokakaov@mail.ru>
2026-07-29 18:41:47 +0300

    feat: added habit list/retrieve Api view

--- habits/urls.py
+++ habits/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from habits.views import HabitsListApiView, HabitRetrieveApiView
 
 app_name = 'habits'
 
-urlpatterns = []
+urlpatterns = [
+    path('list/', HabitsListApiView.as_view(), name='list'),
+    path('<int:pk>/>', HabitRetrieveApiView.as_view(), name='detail'),
+]
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.db.models import Q
+from drf_spectacular.utils import extend_schema
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from habits.models import Habit
+from habits.serializers import HabitSerializer, PublicHabitSerializer
+
+
+@extend_schema(
+    tags=['Привычки'],
+    summary='Список общедоступных привычек',
+    description='Любой авторизованный пользователь может просматривать публичные привычки других пользователей.'
+)
+class HabitsListApiView(ListAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True).exclude(user=self.request.user)
+
+
+@extend_schema(
+    tags=['Привычки'],
+    summary='Детали привычки',
+    description='Пользователь может посмотреть детали своей привычки или любой публичной привычки.'
+)
+class HabitRetrieveApiView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(
+                    Q(user=self.request.user) | Q(is_public=True)
+                )
+
+    def get_serializer_class(self):
+        habit = self.get_object()
+        if habit.is_public and habit.user != self.request.user:
+            return PublicHabitSerializer
+        return HabitSerializer

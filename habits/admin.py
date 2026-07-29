93933c08e7faf18086da4e6166f6a9be3f605be6
Vlad Spinul <shinokakaov@mail.ru>
2026-07-29 16:40:23 +0300

    feat: habit and customuser models registered in admin

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'action', 'place', 'time', 'is_pleasant', 'is_public', 'periodicity')
+    search_fields = ('action', 'place', 'user__email')
+    list_filter = ('is_pleasant', 'is_public', 'periodicity')
+    ordering = ('-id',)
